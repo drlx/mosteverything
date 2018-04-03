f0107b242bc91b39a1d70af4ee62bbdf414358c9
Yazid Mehenni <yazid129@gmail.com>
2018-04-03 17:27:32 -0400

    add problem 10

--- decode/02_test-cases-practice/problems/problem10.js
+++ decode/02_test-cases-practice/problems/problem10.js
@@ -2,11 +2,20 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
+    'ALL CAPS TEST',
+    'no caps test',
+    'Already Proper Test',
+    'ABCdefgHIJ',
+    'many DIFFERENT CaSes HERE'
   
 ]
 
 let outputs = [
-  
+  'All Caps Test',
+  'No Caps Test',
+  'Already Proper Test',
+  'Abcdefghij',
+  'Many Different Cases Here'
 ]
 
 /*
@@ -17,7 +26,22 @@ f("ALL YOUR BASE ARE BELONG"); // All Your Base Are Belong
 
 */
 function f(str) {
-    
+    //Make all lowercase 
+    str = str.toLowerCase();
+    console.log('I made str lower case -->', str);
+    //Split string into words
+    var words = str.split(" ");
+    //console.log('I split str into words --->',words);
+    for (i=0;i<words.length;i++){
+    var letters = words[i].split('');
+    letters[0] = letters[0].toUpperCase();
+    letters = letters.join('');
+    words[i] = letters;
+    }
+    str = words.join(" ");
+    console.log('result ---> ', str)
+    return str;
+
 }
 
 function runTest(i) {
