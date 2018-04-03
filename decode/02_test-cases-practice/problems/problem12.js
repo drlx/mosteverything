3b669a02ed729c8640a5243989bafc895b5b80c8
Yazid Mehenni <yazid129@gmail.com>
2018-04-03 18:02:52 -0400

    problem 12

--- decode/02_test-cases-practice/problems/problem12.js
+++ decode/02_test-cases-practice/problems/problem12.js
@@ -2,11 +2,19 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
-  
+  [[1,2,3,4,5],[1,2,3,4,5]],
+  [[1,2,3,4,5],[6,7,8,9]],
+  [[1,2,3,4,5],[5,4,2,1,9]],
+  [[undefined,2,5],['test',4,7,8]],
+  [[1,2,3,4,5],[8,8,8,4,8]]
 ]
 
 let outputs = [
-  
+  [1,2,3,4,5],
+  [],
+  [1,2,4,5],
+  undefined,
+  [4]
 ]
 
 /*
@@ -19,8 +27,25 @@ uniqueElements([0,1,2,3], [1,3,4,5]); // [0,4,5]
 uniqueElements([1,2,3], [1,2,3]); // []
 uniqueElements(2,3); // undefined, not arrays
 */
-function f(arr1, arr2) {
-    
+function f(arr1) {
+var temp = arr1;
+var arr1 = temp[0];
+var arr2 = temp[1];
+
+var same = [];
+for (i=0;i<arr1.length;i++){
+  for (n=0;n<arr2.length;n++){
+      if (typeof arr1[i] != 'number'){
+          return undefined
+      }
+      else if (arr1[i] == arr2[n]){
+          same.push(arr1[i]); 
+      }
+  }
+
+}
+console.log(same);
+return same;
 }
 
 function runTest(i) {
@@ -29,6 +54,7 @@ function runTest(i) {
     var actual = f(inputs[i]);
     assert.deepEqual(actual, expected);
 }
+
 
 runTest(0);
 runTest(1);
