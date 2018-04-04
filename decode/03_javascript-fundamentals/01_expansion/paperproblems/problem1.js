b0a09c16a31591519aa95732d603366e52682a05
Yazid Mehenni <yazid129@gmail.com>
2018-04-04 16:06:55 -0400

    expansion problem

--- decode/03_javascript-fundamentals/01_expansion/paperproblems/problem1.js
+++ decode/03_javascript-fundamentals/01_expansion/paperproblems/problem1.js
@@ -19,10 +19,50 @@ function reverseString(str) {
 }
 // Expand each of the following and get the result of the expression
 // #1
-square(decrement(square(decrement(3))))
+console.log('problem one',square(decrement(square(decrement(3)))))
+
+var x = decrement(3);
+
+var y = square(x);
+
+var z = decrement(y);
+
+var w = square(z);
+
+console.log('problem one',w);
+
 // #2
-decrement(decrement(square(square(3))))
+console.log('problem two', decrement(decrement(square(square(3)))));
+
+var a = square(3);
+
+var b = square(a);
+
+var c = decrement(b);
+
+var d = decrement(c);
+
+console.log('problem two', d);
+
+
 // #3
-duplicateString(reverseString("hello"))
+console.log('problem three',duplicateString(reverseString("hello")))
+
+var aa = reverseString('hello');
+
+var bb = duplicateString(aa);
+
+console.log('problem three', bb);
+
 // #4
-reverseString(duplicateString(duplicateString("foo")))
+console.log('problem four', reverseString(duplicateString(duplicateString("foo"))))
+
+var ww = 'foo'
+
+var zz = duplicateString(ww);
+
+var yy = duplicateString(zz);
+
+var xx = reverseString(yy);
+
+console.log('problem four',xx);
